ac9e5efdb92f72c7c60a2aceb9d9a617bf62ac6c
prasanthAmbikapathi <prasanth06a@gmail.com>
2022-12-21 10:48:01 +0530

    my task

---
+++ src/gitday/Git.java
@@ -0,0 +1,13 @@
+package gitday;
+
+public class Git {
+
+	public void m1() {
+
+		System.out.println("prasanthvaisu");
+	}
+	public static void main(String[] args) {
+		Git g = new Git();
+		g.m1();
+	}
+}
